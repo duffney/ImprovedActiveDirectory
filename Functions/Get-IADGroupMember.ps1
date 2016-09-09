fb1146efaa5b30896705ba3e2b398d93b01dcc1b
Josh Duffney <duffney@outlook.com>
2016-09-09 13:04:25 -0500

    completed comment based help

--- Functions/Get-IADGroupMember.ps1
+++ Functions/Get-IADGroupMember.ps1
@@ -2,10 +2,23 @@ function Get-IADGroupMember
 {
 <#
 .SYNOPSIS
+    Gets the members of an Active Directory group.
 .DESCRIPTION
+    Get-IADGroupMember gets the members of an Active Directory group. Members include users, groups, computers and foreignSecurityPrincipal objects.
+    It improves upon the Get-ADGroup cmdlet by supporting cross forest direct and nested memberships as well as the ability to report back foreignSecurityPrincipal
+    or "broken SIDS" objects.
 .PARAMETER Identity
+    Specifies an Active Directory object by GUID, DistinguishedName, objectSID, or SamAccountName.
+.PARAMETER Server
+    Specifies the Active Directory Domain Services instance to connect to.
+.PARAMETER Recursive
+    Specifies that the cmdlet report all nested groups and group memberships.
 .EXAMPLE
+    Get-IADGroupMember -Identity Group1
 .EXAMPLE
+    Get-IADGroupMember -Identity Group1 -Server Globomantics.com
+.EXAMPLE
+    Get-IADGroupMember -Identity Group1 -Server Globomantics.com -Recursive    
 #>       
     [CmdletBinding()]
     [Alias()]
