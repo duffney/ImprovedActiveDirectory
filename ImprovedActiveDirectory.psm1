c8e956e561aa472d0db69b18612e99680c9963eb
Nate Foreman <nkforeman@gmail.com>
2016-09-26 17:42:08 -0500

    added empty-iadcomputerscontainer

--- ImprovedActiveDirectory.psm1
+++ ImprovedActiveDirectory.psm1
@@ -12,9 +12,11 @@ catch [System.Management.Automation.ParameterBindingException] {
 . $PSScriptRoot\Functions\Disable-ADUser.ps1
 . $PSScriptRoot\Functions\ConvertTo-IADDomain.ps1
 . $PSScriptRoot\Functions\Move-IADDisabledUsers.ps1
+. $PSScriptRoot\Functions\Empty-IADComputersContainer.ps1
 
 Export-ModuleMember ConvertTo-IADDomain
 Export-ModuleMember Get-IADGroupMember
 Export-ModuleMember Disable-ADComputer
 Export-ModuleMember Disable-ADUser
 Export-ModuleMember Move-IADDisabledUsers
+Export-ModuleMember Empty-IADComputersContainer
