fced5fd59bc652baf84ed9366e9015657319a3ac
Nate Foreman <nkforeman@gmail.com>
2016-09-28 10:19:59 -0500

    9/28/2016

--- Functions/Disable-IADInactiveComputers.ps1
+++ Functions/Disable-IADInactiveComputers.ps1
@@ -1,27 +1,35 @@
 ﻿Function Disable-IADInactiveComputers{
 <#
 .SYNOPSIS
-    Moves all inactive computer accounts found inside a domain to the specified OU for disabled computers. 
+    Disables all inactive computer accounts found inside a domain to the specified OU for disabled computers. 
 .DESCRIPTION
-    Move-IADDisabledComputers queries the specified domain, gathers all inactive computer accounts, and moves them to the Active Directory path provided by operator. Inactivity is determined using the lastlogontimestamp of the computer. Suggested application of this function is to use it inside a daily automated task to ensure a more organized Active Directory. 
+    Disable-IADInactiveComputers queries the specified domain, gathers all inactive computer accounts, and moves them to the Active Directory path provided by operator. Inactivity is determined using the lastlogontimestamp of the computer. Default behavior of the command will target Workstation Operating Systems only (Windows 7, 8, 10, and XP). Suggested application of this function is to use it inside a daily automated task to ensure a more organized Active Directory. 
 .PARAMETER Server
     Specifies the Active Directory Domain Services instance to connect to.
 .PARAMETER Credential
     Specifies the credentials used to move the objects. Functions the same way as Active Directory cmdlets (PSCredential). 
 .PARAMETER DisabledOU
     Specifies the target organizational unit to move the objects to. This parameter accepts the distinguishedname of the OU as a string.
+.PARAMETER DaysInactive
+    Specifies the amount of days of inactivity to target. Accepts an integer for input and checks against the lastlogontimestamp of the computer account.
+.PARAMETER ExclusionList
+    Specifies what machine names to exclude from the command. Input the name property of a computer account and it will leave it out of the query. Accepts string array input. 
+.PARAMETER IncludeServers
+    Supports the SwitchParameter IncludeServers, which tells the command to also query and disable inactive Windows Server machines.
+.PARAMETER IncludeAll
+    Supports the SwitchParameter IncludeAll, which tells the command to query for ALL computer accounts that are inactive. This may include Mac OSX, Linux, Storage devices, etc. Be careful using this as some machine types joined may not have functionality to update their lastlogontimestmap but require domain membership.
 .PARAMETER WhatIf
     Supports the SwitchParameter WhatIf. If specified the function will return what would happen if it was ran, taking no action. Use this to see what accounts are targeted without actually moving them.
 .PARAMETER Confirm
     Supports the SwitchParameter Confirm. If specified the function will ask for confirmation before moving each account. If you do not specify -Confirm it will move the objects without asking.
 .EXAMPLE
-    Move-IADDisabledComputers -Server fabrikom.com -Credential (get-credential) -DisabledOU "OU=Disabled,DC=fabrikom,DC=com" -Confirm
+    Disable-IADInactiveComputers -Server fabrikom.com -Credential (Get-Credential) -DisabledOU "OU=Disabled,DC=fabrikom,DC=com" -DaysInactive 90 -WhatIf -IncludeServers
 
-    Queries the fabrikom.com domain after prompting for credentials to use. Will ask for confirmation before moving to the Disabled OU specified.
+    Queries the fabrikom.com domain after prompting for credentials to use. Includes both desktop and server computer objects that have not logged on for 90 days. Returns what will happen, but does not take action (-WhatIf).  
 .EXAMPLE
-    Move-IADDisabledComputers -Credential $Creds -DisabledOU "OU=Disabled,DC=fabrikom,DC=com" -WhatIf
+    Disable-IADInactiveComputers -Server fabrikom.com -Credential $Creds -DisabledOU "OU=Disabled,DC=fabrikom,DC=com" -DaysInactive 180
 
-    Queries the currently joined domain using credentials stored inside the $Creds PSCredential object and returns what will happen, but does not take action (-WhatIf).    
+    Queries the currently joined domain using credentials stored inside the $Creds PSCredential object. Queries and disables all desktop computers that have not logged on for 180 days.  
 #>       
     [CmdletBinding()]
     param([string]$Server,
@@ -54,7 +62,7 @@
     $FilterString = "$FilterExclusionList"
 
     #Gather the inactive computer accounts
-    if($IncludeServers -and $IncludeAll){throw "You may only specify one: -IncludeServers, -IncludeAll, or default behavior"}
+    if($IncludeServers -and $IncludeAll){throw "You may only specify one option: -IncludeServers, -IncludeAll, or neither for default behavior"}
 
     if($IncludeAll){
         if($FilterString){$InactiveComputers = Get-ADComputer -Server $Server -Filter $FilterString -Properties lastlogontimestamp,description,operatingsystem `
@@ -116,6 +124,6 @@
 
     else{$ReturnList = "No inactive computers found"}
 
-    return $ReturnList
+    return $ReturnList 
 }
 
