b32723a6fa438960f2e89888b586fef6c4f5162c
Nathan Foreman <nkforeman@gmail.com>
2016-11-07 12:04:39 -0600

    Revert "Revert "Added Move-IADEnabledComputers""
    This reverts commit 492760905ad17875bcdd4bb53d7d8ea45078dbde.

--- ImprovedActiveDirectory.psm1
+++ ImprovedActiveDirectory.psm1
@@ -13,6 +13,7 @@ catch [System.Management.Automation.ParameterBindingException] {
 . $PSScriptRoot\Functions\ConvertTo-IADDomain.ps1
 . $PSScriptRoot\Functions\Move-IADDisabledUsers.ps1
 . $PSScriptRoot\Functions\Move-IADDisabledComputers.ps1
+. $PSScriptRoot\Functions\Move-IADEnabledComputers.ps1
 . $PSScriptRoot\Functions\Empty-IADComputersContainer.ps1
 . $PSScriptRoot\Functions\Disable-IADInactiveComputers.ps1
 
@@ -22,5 +23,6 @@ Export-ModuleMember Disable-ADComputer
 Export-ModuleMember Disable-ADUser
 Export-ModuleMember Move-IADDisabledUsers
 Export-ModuleMember Move-IADDisabledComputers
+Export-ModuleMember Move-IADEnabledComputers
 Export-ModuleMember Empty-IADComputersContainer
 Export-ModuleMember Disable-IADInactiveComputers
